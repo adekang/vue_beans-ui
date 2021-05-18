68ca36b747c5db61fa576f4034b7c96e337fe253
adekang <201497709@qq.com>
2021-05-18 18:15:12 +0800

    封装TopNav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -14,9 +11,11 @@
     </div>
   </div>
 </template>
-<style lang="scss" scoped>
-.topnav {
-  border: 1px solid red;
-}
 
-</style>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue';
+
+export default {
+  components: {Topnav}
+};
+</script>
