3436263ce595719380cc52c7c7fb069e1e1fa78f
adekang <201497709@qq.com>
2021-05-18 21:41:42 +0800

    switch组件样式添加样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -28,5 +28,14 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
+}
+
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
 </style>
