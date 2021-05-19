94f5168707c668949c3d0142ccf485586fc38f14
adekang <201497709@qq.com>
2021-05-19 20:23:00 +0800

    Button 的基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Button组件</div>
+  <div>Button示例</div>
+  <Button> 你好</Button>
+  <Button theme="button"> 你好</Button>
+  <Button theme="link"> 你好</Button>
+  <Button theme="text"> 你好</Button>
+
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
 
+export default {
+  components: {
+    Button
+  },
 
-export default {};
+  setup() {
+    const onClick = () => {
+      console.log('点击');
+    };
+
+    return {onClick};
+  }
+};
 </script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="gulu-button" :class="{[`theme-${theme}`]:theme}">
+      <slot/>
+    </button>
+
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  },
+  setup() {}
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
