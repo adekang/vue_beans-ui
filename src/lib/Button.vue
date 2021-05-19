d8189dd76ffa389abea26322c826334023333979
adekang <201497709@qq.com>
2021-05-19 21:25:42 +0800

    支持level and  size

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
     size: {
       type: String,
       default: 'normal'
-    }
+    },
+    level: {
+      type: String,
+      default: 'normal',
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`beans-theme-${theme}`]: theme,
         [`beans-size-${size}`]: size,
+        [`beans-level-${level}`]: level,
       };
     });
 
@@ -39,6 +44,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .beans-button {
   box-sizing: border-box;
   height: $h;
@@ -53,6 +59,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -102,6 +109,63 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+
+  &.beans-theme-button {
+    &.beans-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.beans-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.beans-theme-link {
+    &.beans-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.beans-theme-text {
+    &.beans-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.beans-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 
 }
