856e626ecb4a642a68a7388f657e700c4f2ac485
adekang <201497709@qq.com>
2021-05-20 16:48:09 +0800

    完成 Tabs组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>TAB 案例</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">示例一</Tab>
     <Tab title="导航2">示例二</Tab>
   </Tabs>
@@ -9,8 +9,14 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Tab, Tabs}
+  components: {Tab, Tabs},
+  setup() {
+    const x = ref('导航1');
+
+    return {x};
+  }
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,37 +1,84 @@
 <template>
   <div class="beans-tabs">
-    <div class="beans-tabs-nav">
-      <div class="beans-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    <div class="beans-tabs-nav" ref="container">
+      <div
+          :class="{ selected: t === selected }"
+          :ref="(el) => {if (t === selected) selectedItem = el;}"
+          @click="select(t)"
+          class="beans-tabs-nav-item"
+          v-for="(t, index) in titles"
+          :key="index"
+      >
+        {{ t }}
+      </div>
+      <div class="beans-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="beans-tabs-content">
-      <component class="beans-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component
+          class="beans-tabs-content-item"
+          :class="{ selected: c.props.title === selected }"
+          v-for="(c,index) in defaults"
+          :is="c"
+          :key="index"
+      />
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Tab from './Tab.vue';
+import {ref, onMounted, onUpdated} from 'vue';
 
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
+    // 导航线条的移动
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+
+    const x = () => {
+      const {width} = selectedItem.value.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = container.value.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    };
+
+    onMounted(x);
+    onUpdated(x);
+
+    // 判断组件是Tab
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab');
       }
     });
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
     return {
       defaults,
-      titles
+      titles,
+      select,
+      selectedItem,
+      indicator,
+      container,
     };
-  }
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang='scss'>
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
@@ -40,6 +87,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -54,10 +102,28 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
   }
 
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
-</style>s
+</style>
