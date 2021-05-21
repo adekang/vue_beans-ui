825e53d3e3ee7593629c5ed2d160aec686191e34
adekang <201497709@qq.com>
2021-05-21 21:18:46 +0800

    重新创建版本 修复md.ts bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 // @ts-nocheck
 
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
+
 import {md} from './plugins/md';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), md()]
-});
+export default {
+  plugins: [md()]
+};
