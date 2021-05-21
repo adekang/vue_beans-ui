49da925ac1755490ffea1dca0cfb80ee75089486
adekang <201497709@qq.com>
2021-05-21 20:56:55 +0800

    制作 md.ts 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// @ts-nocheck
+
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import {md} from './plugins/md';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), md()]
+});
