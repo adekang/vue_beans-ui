f976abc7d4eb73f6b76cb93487bee8f6b355de20
adekang <201497709@qq.com>
2021-05-21 21:40:48 +0800

    优化md文件

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,11 @@ import SwitchDemo from './components/SwitchDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import DocDemo from './components/DocDemo.vue';
-import Intro from './views/Intro.vue';
-import Install from './views/Install.vue';
-import GetStarted from './views/GetStarted.vue';
+import {h} from 'vue';
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
+const md = (filename: string) => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
 export const router = createRouter({
   history: history,
   routes: [
@@ -18,9 +18,9 @@ export const router = createRouter({
     {
       path: '/doc', component: Doc, children: [
         {path: '', component: DocDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md('intro')},
+        {path: 'get-started', component: md('get-started')},
+        {path: 'install', component: md('install')},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
@@ -28,8 +28,4 @@ export const router = createRouter({
       ]
     }
   ]
-});
-
-router.afterEach(() => {
-  console.log('路由切换了');
 });
