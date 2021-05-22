13c9f95fcd784a3fb98dda19bed81f98da2305f1
adekang <201497709@qq.com>
2021-05-22 10:37:56 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+ #!/usr/bin/env sh
+
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:adekang/vue_beans-ui.git &&
+git push -f -u origin master &&
+cd -
