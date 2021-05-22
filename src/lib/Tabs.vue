a9d9596266064c00c2c9b1247c38b38c9bac5362
adekang <201497709@qq.com>
2021-05-22 11:17:06 +0800

    修复正式版 3.0的bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -27,7 +27,7 @@
 
 <script lang='ts'>
 import Tab from './Tab.vue';
-import {ref, onMounted, onUpdated} from 'vue';
+import {ref, onMounted, watchEffect} from 'vue';
 
 export default {
   props: {
@@ -41,17 +41,24 @@ export default {
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
 
-    const x = () => {
-      const {width} = selectedItem.value.getBoundingClientRect();
-      indicator.value.style.width = width + 'px';
-      const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = selectedItem.value.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left + 'px';
-    };
-
-    onMounted(x);
-    onUpdated(x);
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + 'px';
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect();
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + 'px';
+      }, {
+        flush: 'post'
+      });
+    });
 
     // 判断组件是Tab
     const defaults = context.slots.default();
