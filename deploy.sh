4528e556ceea7b9567ee0fd502676856115443a2
adekang <201497709@qq.com>
2021-05-22 16:41:54 +0800

    修改部署位置

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:adekang/vue_beans-ui.git &&
+git remote add origin git@github.com:adekang/vue_beans-ui-web.git &&
 git push -f -u origin master &&
 cd -
